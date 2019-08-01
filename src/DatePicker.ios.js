284571289624a2edc9be6aaa06532d429bc62554
hduprat <hugod@bam.tech>
2019-08-01 18:59:14 +0200

    to merge with initialDate

--- src/DatePicker.ios.js
+++ src/DatePicker.ios.js
@@ -3,44 +3,45 @@
  * @flow
  * */
 
-import React from 'react'
-import { DatePickerIOS, StyleSheet, Dimensions } from 'react-native'
+import React from "react";
+import { DatePickerIOS, StyleSheet, Dimensions } from "react-native";
 
 type Props = {
   onDateSelected: Date => void,
-  initDate: Date
-}
+  initialDate: Date
+};
 
 type State = {
   chosenDate: Date
-}
+};
 
 export default class DatePicker extends React.Component<Props, State> {
-  constructor(props: Props){
-    super(props)
-    this.state = { chosenDate: props.initDate || new Date() }
+  constructor(props: Props) {
+    super(props);
+    this.state = { chosenDate: props.initialDate || new Date() };
   }
 
-  setDate = (newDate) => {
-    this.setState({chosenDate: newDate})
-    const { onDateSelected } = this.props
-    if (onDateSelected) onDateSelected(newDate)
-  }
+  setDate = newDate => {
+    this.setState({ chosenDate: newDate });
+    const { onDateSelected } = this.props;
+    if (onDateSelected) onDateSelected(newDate);
+  };
 
-  render(){
+  render() {
     return (
-        <DatePickerIOS
-          style={styles.picker}
-          date={this.state.chosenDate}
-          onDateChange={this.setDate}
-          {...this.props}
-        />
-    )
+      <DatePickerIOS
+        style={styles.picker}
+        date={this.state.chosenDate}
+        onDateChange={this.setDate}
+        initialDate={this.props.initialDate}
+        {...this.props}
+      />
+    );
   }
 }
 
 let styles = StyleSheet.create({
   picker: {
-    width: Dimensions.get('screen').width
-  },
-})
+    width: Dimensions.get("screen").width
+  }
+});
