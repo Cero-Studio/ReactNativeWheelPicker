c7ca6d19b9f1f0036e867d3184eff5500e01b21f
hduprat <hugod@bam.tech>
2019-08-03 11:00:15 +0200

    implement tests

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current"
+        }
+      }
+    ],
+    "module:metro-react-native-babel-preset"
+  ]
+};
---
+++ src/tests/Utils.test.js
@@ -0,0 +1,9 @@
+import { computeDatePosition } from "../Utils";
+
+describe("Utils", () => {
+  describe("computeDatePosition", () => {
+    it("works", () => {
+      expect(true).toBeTruthy();
+    });
+  });
+});
