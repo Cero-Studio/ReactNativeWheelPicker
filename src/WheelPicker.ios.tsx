e76a12dc5f687dcbf5f94b816c9faa5e7573a309
teerryn <slaughterer90@gmail.com>
2020-10-02 12:53:35 +0100

    Remove picker props type

--- src/WheelPicker.ios.tsx
+++ src/WheelPicker.ios.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { View } from "react-native";
 import { Picker } from "@react-native-community/picker";
-import { PickerItemProps } from "@react-native-community/picker/typings/Picker";
 
-type Props = {
+interface Props {
   data: Array<string>;
   selectedItem?: number;
   onItemSelected?: Function;
   disabled?: boolean;
-} & PickerItemProps;
+}
 
 const WheelPicker: React.FC<Props> = props => {
   const [selectedItem, setSelectedItem] = useState(props.selectedItem || 0);
